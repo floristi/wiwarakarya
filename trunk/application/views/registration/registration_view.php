3e835630c1838d0f35f942589df486b8e380c706
fizzter149@gmail.com <fizzter149@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-02-19 11:13:00 +0000

    fix registration form

--- trunk/application/views/registration/registration_view.php
+++ trunk/application/views/registration/registration_view.php
@@ -1,203 +1,230 @@
-<?php $this->load->view('layout/header');?>		
-			<div class="container">
-				<div class="main">
-					<!-- start registration -->
-					<div class="registration">
-						<div class="registration_left">
-						<h2>new user? <span> create a runnerssport account </span></h2>
-						<a href="#"><div class="reg_fb"><img src="<?php echo base_url();?>resources/images/facebook.png" alt=""><i>register using Facebook</i><div class="clearfix"></div></div></a>
-						<!-- [if IE] 
-							< link rel='stylesheet' type='text/css' href='ie.css'/>  
-							[endif] -->  
-				  
-						<!-- [if lt IE 7]>  
-							< link rel='stylesheet' type='text/css' href='ie6.css'/>  
-						<! [endif] -->  
-						<script>
-							(function() {
-						
-							// Create input element for testing
-							var inputs = document.createElement('input');
-							
-							// Create the supports object
-							var supports = {};
-							
-							supports.autofocus   = 'autofocus' in inputs;
-							supports.required    = 'required' in inputs;
-							supports.placeholder = 'placeholder' in inputs;
-						
-							// Fallback for autofocus attribute
-							if(!supports.autofocus) {
-								
-							}
-							
-							// Fallback for required attribute
-							if(!supports.required) {
-								
-							}
-						
-							// Fallback for placeholder attribute
-							if(!supports.placeholder) {
-								
-							}
-							
-							// Change text inside send button on submit
-							var send = document.getElementById('register-submit');
-							if(send) {
-								send.onclick = function () {
-									this.innerHTML = '...Sending';
-								}
-							}
-						
-						})();
-						</script>
-						<div class="registration_form">
-						<!-- Form -->
-							<form id="registration_form" action="contact.php" method="post">
-								<div>
-									<label>
-										<input placeholder="first name:" type="text" tabindex="1" required autofocus>
-									</label>
-								</div>
-								<div>
-									<label>
-										<input placeholder="last name:" type="text" tabindex="2" required autofocus>
-									</label>
-								</div>
-								<div>
-									<label>
-										Address
-									</label>
-									<textarea></textarea>
-								</div>
-								<div>
-									<label>
-										Last Degree
-									</label>
-									<select class="form-control">
-									  <option>SMP</option>
-									  <option>SMA</option>
-									  <option>Diploma D3</option>
-									  <option>Sarjana S1</option>
-									  <option>Magister S2</option>
-									  <option>Doktor S3</option>
-									</select>
-								</div>
-								<div>
-									<label>
-										<input placeholder="email address:" type="email" tabindex="3" required>
-									</label>
-								</div>
-								<div class="sky-form">
-									<label>Interest</label>
-									<div class="sky_form1">
-										<ul>
-											<label class="checkbox-inline">
-												<input type="checkbox" name="inlineCheckboxOptions" id="inlinecheck1" value="option1"> Option1
-											</label>
-											<label class="checkbox-inline">
-												<input type="checkbox" name="inlineCheckboxOptions" id="inlinecheck2" value="option2"> Option2
-											</label>
-											<label class="checkbox-inline">
-												<input type="checkbox" name="inlineCheckboxOptions" id="inlinecheck3" value="option1"> Option3
-											</label>
-											<label class="checkbox-inline">
-												<input type="checkbox" name="inlineCheckboxOptions" id="inlinecheck4" value="option2"> Option4
-											</label>
-											<label class="checkbox-inline">
-												<input type="checkbox" name="inlineCheckboxOptions" id="inlinecheck5" value="option1"> Option5
-											</label>
-											
-											<div class="clearfix"></div>
-										</ul>
-									</div>
-								</div>
-								<div class="sky-form">
-									<label>Gender</label>
-									<div class="sky_form1">
-										<ul>
-											<label class="radio-inline">
-												<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Male
-											</label>
-											<label class="radio-inline">
-												<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Female
-											</label>
-											<div class="clearfix"></div>
-										</ul>
-									</div>
-								</div>
-								<div class="sky-form">
-									<label>Membership Type</label>
-									<div class="sky_form1">
-										<ul>
-											<label class="radio-inline">
-												<input type="radio" name="inlineRadioOptions" id="inlineRadiox1" value="option1"> Free
-											</label>
-											<label class="radio-inline">
-												<input type="radio" name="inlineRadioOptions" id="inlineRadiox2" value="option2"> Premium
-											</label>
-											<div class="clearfix"></div>
-										</ul>
-									</div>
-								</div>
-								<div>
-									<label>Upload CV</label>
-									<label>
-										<input type="file" tabindex="3" required>
-									</label>
-								</div>
-								<div>
-									<label>
-										<input placeholder="password" type="password" tabindex="4" required>
-									</label>
-								</div>						
-								<div>
-									<label>
-										<input placeholder="retype password" type="password" tabindex="4" required>
-									</label>
-								</div>	
-								<div>
-									<input type="submit" value="create an account" id="register-submit">
-								</div>
-								<div class="sky-form">
-									<label class="checkbox-inline">
-										<input type="checkbox" id="inlineCheckbox1" value="option1">I Agree to Wiwara Karya &nbsp;<a class="terms" href="#">terms of service</a>
-									</label>
-								</div>
-							</form>
-							<!-- /Form -->
-							</div>
-						</div>
-						
-						<div class="registration_left">
-							<h2>existing user</h2>
-							<a href="#"><div class="reg_fb"><img src="<?php echo base_url();?>resources/images/facebook.png" alt=""><i>login using Facebook</i><div class="clearfix"></div></div></a>
-							<div class="registration_form">
-								<!-- Form -->
-								<form id="registration_form" action="contact.php" method="post">
-									<div>
-										<label>
-											<input placeholder="email:" type="email" tabindex="3" required>
-										</label>
-									</div>
-									<div>
-										<label>
-											<input placeholder="password" type="password" tabindex="4" required>
-										</label>
-									</div>						
-									<div>
-										<input type="submit" value="sign in" id="register-submit">
-									</div>
-									<div class="forget">
-										<a href="#">forgot your password</a>
-									</div>
-								</form>
-								<!-- /Form -->
-							</div>
-						</div>
-					<div class="clearfix"></div>
-				</div>
-				<!-- end registration -->
-				</div>
-			</div>
-<?php $this->load->view('layout/footer');?>
+<?php $this->load->view('layout/header'); ?>
+    <div class="container">
+        <div class="main">
+            <!-- start registration -->
+            <div class="registration">
+                <div class="registration_left">
+                    <!-- [if IE]
+                        < link rel='stylesheet' type='text/css' href='ie.css'/>
+                        [endif] -->
+
+                    <!-- [if lt IE 7]>
+                        < link rel='stylesheet' type='text/css' href='ie6.css'/>
+                    <! [endif] -->
+                    <script>
+                        (function () {
+
+                            // Create input element for testing
+                            var inputs = document.createElement('input');
+
+                            // Create the supports object
+                            var supports = {};
+
+                            supports.autofocus = 'autofocus' in inputs;
+                            supports.required = 'required' in inputs;
+                            supports.placeholder = 'placeholder' in inputs;
+
+                            // Fallback for autofocus attribute
+                            if (!supports.autofocus) {
+
+                            }
+
+                            // Fallback for required attribute
+                            if (!supports.required) {
+
+                            }
+
+                            // Fallback for placeholder attribute
+                            if (!supports.placeholder) {
+
+                            }
+
+                            // Change text inside send button on submit
+                            var send = document.getElementById('register-submit');
+                            if (send) {
+                                send.onclick = function () {
+                                    this.innerHTML = '...Sending';
+                                }
+                            }
+
+                        })();
+                    </script>
+                    <div class="registration_form">
+                        <h1>Register</h1>
+                        <br/>
+                        <div style="font-size:1.5em">Are you a job <b><em>poster</em></b> or a job <b><em>hunter</em></b>?</div>
+                        <select id="user-role" class="form-control">
+                            <option value="0">Job Hunter</option>
+                            <option value="1">Job Poster</option>
+                        </select>
+                        <script type="text/javascript">
+                            $("#user-role").change(function() {
+                                var role = $("#user-role").val();
+                                if (role == 0) {
+                                    $("#company-container").fadeOut(500);
+                                    $("#applicant-container").fadeIn(500);
+                                } else if (role == 1) {
+                                    $("#company-container").fadeIn(500);
+                                    $("#applicant-container").fadeOut(500);
+                                }
+                            });
+                        </script>
+                        <br/><br/>
+                        <div id="applicant-container">
+                            <form id="applicant_registration_form" action="" method="post">
+                                <div>
+                                    <label>
+                                        Username:
+                                        <input placeholder="username" type="text" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Password:
+                                        <input placeholder="password" type="password" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Full Name:
+                                        <input placeholder="full name" type="text" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Place of Birth:
+                                    </label>
+                                    <textarea placeholder="place of birth" class="form-control"></textarea>
+                                </div>
+
+                                <div>
+                                    <label>
+                                        Date of Birth:
+                                    </label>
+                                    <select class="inline-group">
+                                        <?php
+                                            for($i=1; $i<=31; $i++)
+                                                echo("<option>".$i."</option>");
+                                        ?>
+                                    </select>
+                                    <select class="inline-group">
+                                        <option>Jan</option>
+                                        <option>Feb</option>
+                                        <option>Mar</option>
+                                        <option>Apr</option>
+                                        <option>May</option>
+                                        <option>Jun</option>
+                                        <option>Jul</option>
+                                        <option>Aug</option>
+                                        <option>Sep</option>
+                                        <option>Oct</option>
+                                        <option>Nov</option>
+                                        <option>Dec</option>
+                                    </select>
+                                    <select class="inline-group">
+                                        <?php
+                                        for($i=1970; $i<=2015; $i++)
+                                            echo("<option>".$i."</option>");
+                                        ?>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>
+                                        CV:
+                                    </label>
+                                    <input type="file" required autofocus>
+                                </div>
+                                <div class="sky-form">
+                                    <label class="checkbox-inline">
+                                        <input type="checkbox" value="option1">I Agree to Wiwara Karya
+                                        &nbsp;<a class="terms" href="#">terms of service</a>
+                                    </label>
+                                </div>
+                                <div>
+                                    <input type="submit" value="create an account" id="register-submit">
+                                </div>
+                            </form>
+                        </div>
+
+                        <div id="company-container" style="display:none">
+                            <!-- Applicant Registration Form -->
+                            <form id="company_registration_form" action="" method="post">
+                                <div>
+                                    <label>
+                                        Username:
+                                        <input placeholder="username" type="text" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Password:
+                                        <input placeholder="password" type="password" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Company Name:
+                                        <input placeholder="company name" type="text" required autofocus>
+                                    </label>
+                                </div>
+                                <div>
+                                    <label>
+                                        Address:
+                                    </label>
+                                    <textarea placeholder="address" class="form-control"></textarea>
+                                </div>
+                                <div>
+                                    <label>
+                                        City:
+                                    </label>
+                                    <input placeholder="city" type="text" required autofocus>
+                                </div>
+                                <div>
+                                    <label>
+                                        Phone Number:
+                                    </label>
+                                    <input placeholder="phone number" type="text" required autofocus>
+                                </div>
+                                <div>
+                                    <label>
+                                        Contact Name:
+                                    </label>
+                                    <input placeholder="contact name" type="text" required autofocus>
+                                </div>
+                                <div>
+                                    <label>
+                                        Description:
+                                    </label>
+                                    <textarea class="form-control" placeholder="description" type="text" required autofocus></textarea>
+                                </div>
+                                <div>
+                                    <label>
+                                        Email:
+                                    </label>
+                                    <input class="form-control" placeholder="email" type="email" required autofocus>
+                                </div>
+                                <div>
+                                    <label>
+                                        Photo:
+                                    </label>
+                                    <input type="file" required autofocus>
+                                </div>
+                                <div class="sky-form">
+                                    <label class="checkbox-inline">
+                                        <input type="checkbox" value="option1">I Agree to Wiwara Karya
+                                        &nbsp;<a class="terms" href="#">terms of service</a>
+                                    </label>
+                                </div>
+                                <div>
+                                    <input type="submit" value="create an account" id="register-submit">
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /Form -->
+                    </div>
+                </div>
+            </div>
+            <!-- end registration -->
+        </div>
+    </div>
+<?php $this->load->view('layout/footer'); ?>
