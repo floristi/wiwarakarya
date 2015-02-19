0fde778a5429e2ed7e3f07814cffe3e035495c8b
fizzter149@gmail.com <fizzter149@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-02-19 11:14:01 +0000

    create company model

---
+++ application/models/m_company.php
@@ -0,0 +1,20 @@
+<?php
+
+class M_company extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    function insert_company($data) {
+        $companyData = null;
+        $userData = null;
+
+        // fill company & user data
+
+        $this->db->insert('users', $userData);
+        $this->db->insert('companies', $companyData);
+    }
+
+}
