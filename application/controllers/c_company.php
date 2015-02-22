d48a8803511c69a4b210c45d2c3f72d64f091537
floristi92@gmail.com <floristi92@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-02-22 12:19:21 +0000

    perbaikan controllers/c_company.php

--- application/controllers/c_company.php
+++ application/controllers/c_company.php
@@ -1,42 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class C_Company extends CI_Controller{
 
-		public function __construct() 
-		{
-			parent::__construct();
-			$this->load->library('session');
-			$this->load->helper('url');
-			$this->load->model('m_company');
-			//$this->check_is_validated();
+	public function __construct() 
+	{
+		parent::__construct();
+		$this->load->library('session');
+		$this->load->helper(array('url','form'));
+		$this->load->model('m_company');
+		//$this->check_is_validated();
+	}
+
+	public function index() 
+	{
+		$this->get_company();
+		//$this->load->view('welcome_message');
+		//$this->load->view('company/company_view');
+	}
+
+
+	function create_company() {
+		$isi['isi'] = 'company/create_company_view';
+		//$isi['confirm'] = $this->session->userdata('notification');
+		//$this->session->set_userdata('notification', null);
+		if ($this->input->post('submit')) {
+			$this->m_company->insert_company();
+			redirect('c_company');
 		}
-
-		public function index() 
-		{
-			$this->get_company();
-			//$this->load->view('welcome_message');
-			//$this->load->view('company/company_view');
-		}
-
-		//public function create_company() 
-		//{
-			//$isi['isi'] = 'company/create_company_view';
-			//$isi['confirm'] = $this->session->userdata('notification');
-			//$this->session->set_userdata('notification', null);
-		//	$this->load->view('company/create_company_view');
-		//}
-
-	/** input_company()
-	*/
-	function input_company() {
-		//$cek = $this->validation();
-
-		//if ($cek == TRUE) {
-			$this->m_company->insert_company();	
-			$this->load->view('company/create_company_view');
-			//redirect('company/create_company_view');
-		//}
-		
-		// $this->create_company();
+		$this->load->view('company/create_company_view', $isi);
 	}
 
 	function search_company($under = null) {
@@ -66,7 +56,32 @@ class C_Company extends CI_Controller{
 		$this->load->view('company/company_view', $isi);
 	}
 
+	function process($id = null) {
+	    if(isset($_POST["update"])) {
+	    	$this->m_company->update_data_company($id);
+	    } else if(isset($_POST["delete"])) {
+	    	$this->m_company->delete_company($id);
+	    } else if(isset($_POST["activate"])) {
+	    	$this->m_company->company_activation($id);
+	    }
+			
+		redirect('c_company/get_company');	
+	}
+	
+	function data_company($id = null) {
+		$isi['query'] = $this->m_company->get_data_company($id);
+		//$isi['confirm'] = $this->session->userdata('notification');
+		//$this->session->set_userdata('notification', null);
+		$isi['isi'] = 'company/update_company';
+		$this->load->view('company/update_company', $isi);
+	}
 
+	/** activate()
+	*/
+	function activate($id = null) {
+		$this->m_company->company_activation($id);
+		redirect('c_company/get_company');
+	}
 	
 }
 ?>
