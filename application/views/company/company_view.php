e2d11395da470452d0e3e2bf0a5fb39150354c4d
fizzter149@gmail.com <fizzter149@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-02-24 23:35:44 +0000

    fix modal

--- application/views/company/company_view.php
+++ application/views/company/company_view.php
@@ -16,54 +16,58 @@
 		</thead>
 		
 		<tbody>
-			<?php
-				foreach ($query as $row) {
-					echo "<tr>";
-					$id = $row->id; 
-					echo "<td>". "<a href='". site_url('c_company/data_company/'.$id) . "' data-toggle=\"modal\">". $row->name ."</a>". "</td>";
-					echo "<td>". $row->address . "</td>";
-					echo "<td>". $row->email . "</td>";
-					echo "<td>". $row->phone . "</td>";
-					$status = $row->status;
-					$stat = "Tidak Aktif";
-					if ($status == 't') {
-						echo "<td><i class=\"icon-ok-sign\"></i> Aktif</td>";
-						$stat = "Aktif";
-					} else
-						echo "<td><i class=\"icon-remove-sign\"></i> Tidak Aktif</td>";
-					echo "</tr>";
-
-					echo 
-						"<div id='". $id ."' class=\"modal hide fade\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
-							<div class=\"modal-header\">
-								<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">×</button>
-								<i class=\"icon-edit\"></i>
-								<h2>Company : ".$row->name."</h2>
-							</div>
-
-								<dt>Alamat<dt>
-								<dd>".$row->address."</dd>
-
-								<dt>Kota<dt>
-								<dd>".$row->city."</dd>
-
-								<dt>Contact Person<dt>
-								<dd>".$row->contact_name."</dd>
-
-								<dt>E-Mail<dt>
-								<dd>".$row->email."</dd>
-
-								<dt>Status<dt>
-								<dd>".$stat."</dd>
-							</dl>
-						</div>
-						<div class=\"modal-footer\">
-							<a href='".site_url('c_company/data_company/'.$id)."'><button class=\"btn btn-primary\" name=\"edit-company\">Edit Company</button></a>
-							<button class=\"btn\" data-dismiss=\"modal\" aria-hidden=\"true\">Close</button>
-						</div>
-					</div>";
-				}
+			<?php foreach ($query as $row) {
+				$id = $row->id; 
+				$status = $row->status;
 			?>
+				<tr>	
+					<td><a href='#' data-toggle="modal" data-target="#detail-<?php echo $id; ?>"><?php echo $row->name; ?></a></td>
+					<td><?php echo $row->address; ?></td>
+					<td><?php echo $row->email; ?></td>
+					<td><?php echo $row->phone; ?></td>
+					<?php if ($status == 't') { ?>
+						<td><i class="icon-ok-sign"></i> Aktif</td>"
+						<?php $stat = "Aktif"; ?>
+					<?php } else { ?>
+						<td><i class="icon-remove-sign"></i> Tidak Aktif</td>
+						<?php $stat = "Tidak Aktif"; ?>
+					<?php } ?>
+				</tr>;
+
+				<div id="detail-<?php echo $id; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="detailCompany" aria-hidden="true">
+				<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<i class="icon-edit"></i>
+						<h2 class="modal-title" id="detailCompany">Company : <?php echo $row->name; ?></h2>
+					</div>
+					<div class="modal-body">
+					<dl>
+						<dt>Alamat<dt>
+						<dd><?php echo $row->address; ?></dd>
+
+						<dt>Kota<dt>
+						<dd><?php echo $row->city; ?></dd>
+
+						<dt>Contact Person<dt>
+						<dd><?php echo $row->contact_name; ?></dd>
+
+						<dt>E-Mail<dt>
+						<dd><?php echo $row->email; ?></dd>
+
+						<dt>Status<dt>
+						<dd><?php echo $stat; ?></dd>
+					</dl>
+					</div>
+					<div class="modal-footer">
+						<a href="<?php echo site_url('c_company/data_company/'.$id); ?>"><button class="btn btn-primary" name="edit-company">Edit Company</button></a>
+						<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+					</div>
+				</div>
+				</div>
+				</div>
+			<?php } ?>
 			</tbody>
 		</table>
 </div>
