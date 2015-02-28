d9471a84e9f9d96ff889962ecf1792320b1ebab3
floristi92@gmail.com <floristi92@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-02-28 10:22:57 +0000

    alosi

--- trunk/application/views/layout/footer.php
+++ trunk/application/views/layout/footer.php
@@ -2,7 +2,7 @@
 		<!-- footer -->
 		<div class="footer">
 			<div class="container">
-				<p>Created By <a href="http://alosi.com" target="_blank">Alosi</a></p>
+				<p>Created By <a href="http://alosi.com" target="_blank">Alosi</a>  --  Powered By <a href="http://alosi.com" target="_blank">Wiwara Karya</a></p>
 			</div>
 		</div>
 		<!-- footer -->
