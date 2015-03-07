ad3b70479e9d5b1a99e4fd96cb0a40e6c9c5dfbf
fizzter149@gmail.com <fizzter149@gmail.com@4bcdd98d-3156-091f-c354-4ad4c0c51424>
2015-03-07 05:11:01 +0000

    update sql

--- trunk/wiwarakarya.sql
+++ trunk/wiwarakarya.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 19 Feb 2015 pada 10.12
+-- Waktu pembuatan: 07 Mar 2015 pada 06.10
 -- Versi Server: 5.5.32
 -- Versi PHP: 5.4.19
 
@@ -31,13 +31,20 @@ USE `wiwarakarya`;
 CREATE TABLE IF NOT EXISTS `applicants` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `last_education` text NOT NULL,
-  `cv_path` text NOT NULL,
-  `pob` text NOT NULL,
-  `dob` datetime NOT NULL,
-  `is_premium` tinyint(1) NOT NULL,
+  `last_education` text,
+  `cv_path` text,
+  `pob` text,
+  `dob` date DEFAULT NULL,
+  `is_premium` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data untuk tabel `applicants`
+--
+
+INSERT INTO `applicants` (`id`, `name`, `last_education`, `cv_path`, `pob`, `dob`, `is_premium`) VALUES
+(6, 'First User', 'S1', NULL, 'Jakarta', '1988-02-02', 0);
 
 -- --------------------------------------------------------
 
@@ -48,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `applicants` (
 CREATE TABLE IF NOT EXISTS `applications` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `applicant_id` bigint(20) NOT NULL,
-  `company_id` bigint(20) NOT NULL,
+  `job_id` bigint(20) NOT NULL,
   `status` text NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -73,7 +80,17 @@ CREATE TABLE IF NOT EXISTS `companies` (
   `email` text NOT NULL,
   `web` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data untuk tabel `companies`
+--
+
+INSERT INTO `companies` (`id`, `name`, `address`, `city`, `phone`, `contact_name`, `status`, `description`, `photo_url`, `email`, `web`) VALUES
+(1, 'PT Astra International, Tbk.', 'Jalan Gaya Motor Raya, Jakarta Utara', 'Jakarta', '0812345678', 'Setyawan Eko', 0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '', 'career@astra-international.com', '0'),
+(2, 'PT Badr Interactive', 'Jalan Juanda Raya, Depok', 'Depok', '0812345678', '  Carson Sammie', 0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '', 'career@badr-interactive.com', '0'),
+(3, 'Samsung R&D Institute Indonesia', 'Wisma GKBI lt 26, Jalan Sudirman, Jakarta', 'Jakarta', '0213456789', '  Sal Mickey', 0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '', 'career@samsungmsci.com', '0'),
+(4, 'PT Integrasi Logistik Cipta Solusi', 'Jalan Yos Sudarso, Jakarta', 'Jakarta', '021345678', '  Leigh Terry', 0, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', '', 'career@ilcs.com', '0');
 
 -- --------------------------------------------------------
 
@@ -91,8 +108,17 @@ CREATE TABLE IF NOT EXISTS `events` (
   `speaker` text NOT NULL,
   `price` text NOT NULL,
   `created_by` bigint(20) NOT NULL,
+  `quota_remaining` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data untuk tabel `events`
+--
+
+INSERT INTO `events` (`id`, `name`, `description`, `location`, `time`, `quota`, `speaker`, `price`, `created_by`, `quota_remaining`) VALUES
+(1, 'Job Fair & Expo', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'Jakarta Convention Center, Hall B', '2015-04-10 08:00:00', 100, '  Florence Shelly', '100000', 4, 100),
+(2, 'Career Path Talk & Seminar', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'Hotel Grand Sahid, Sriwijaya Ballroom', '2015-04-20 08:00:00', 200, '  Jules Beverly', '200000', 3, 200);
 
 -- --------------------------------------------------------
 
@@ -116,7 +142,6 @@ CREATE TABLE IF NOT EXISTS `event_registrations` (
 CREATE TABLE IF NOT EXISTS `jobs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `kuota` int(11) NOT NULL,
   `position` text NOT NULL,
   `position_category` text NOT NULL,
   `due_date` datetime NOT NULL,
@@ -126,7 +151,19 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `tnc` text NOT NULL,
   `created_by` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data untuk tabel `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `name`, `position`, `position_category`, `due_date`, `major`, `last_education`, `salary`, `tnc`, `created_by`) VALUES
+(1, 'Android Developer', 'IT ', 'Staff ', '2015-12-10 23:59:00', 'Computer Science', 'S1', '5000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 3),
+(2, 'System Analyst', 'IT  ', 'Assistant Manager', '2015-12-10 23:59:00', 'Computer Science', 'S1', '6000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 1),
+(3, 'IT Strategic and Planning', 'IT', 'Senior Staff', '2015-04-29 23:59:00', 'Information System', 'S1', '10000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 4),
+(4, 'Public Relation', 'Marketing', 'Manager', '2015-04-15 23:59:00', 'Communication', 'S2', '8000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 2),
+(5, 'Network Engineer', 'IT', 'Staff', '2015-04-20 23:59:00', 'Computer Science', 'S1', '4000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 3),
+(6, 'Quality Assurance', 'IT', 'Senior Staff', '2015-04-10 23:59:00', 'Information Technology', 'S1', '4000000', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 1);
 
 -- --------------------------------------------------------
 
@@ -138,13 +175,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `status` int(11) NOT NULL,
+  `status` text NOT NULL,
   `role` varchar(20) NOT NULL,
-  `company_id` bigint(20) NOT NULL,
   `applicant_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `username` (`username`),
+  KEY `applicants_applicant_id_fk` (`applicant_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `status`, `role`, `applicant_id`) VALUES
+(8, 'admin', 'admin', '', 'ADMIN', 0),
+(9, 'user1', 'user1', '0', 'APPLICANT', 6);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
